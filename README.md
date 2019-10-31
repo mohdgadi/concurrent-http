55269a3705dcc9f3e58ba435ce8506e46d128e6c
Mohammed <mohammed@mohammeds-MacBook-Air.local>
2019-10-31 22:11:52 +0530

    Added goDoc and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # concurrent-http
 A golang micro-libary to make concurrent http request to your server
+
+# Usage
+1) Simply make a net/http Request.
+2) Determine the number of request to be made.
+3) Specify the concurrency i.e parallelism of the requests.
+
+	url := "http://localhost:8080/"
+	req, _ := http.NewRequest("GET", url, nil)
+    numberOfRequests := 100000
+    concurrency := 2500
+
+    <!-- Construct the request -->
+	concurrentRequest := concurrent.NewRequest(req, numberOfRequests, concurrency)
+
+    <!-- Start sending out the request. This is blocking -->
+    resChan :=  concurrentRequest.MakeSync()
+
+    <!-- Get the status i.e percentage of request completed -->
+    percentage := concurrentRequest.Status()
+
+    <!-- Print all the http.Responses and error -->
+    for res := range resChan {
+        httpResponse = res.HttpRespone()
+        error = res.Error()
+    }
--- request.go
+++ request.go
@@ -1,33 +1,42 @@
 package concurrent
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
-	"sync/atomic"
 )
 
+// Request is a concurrent request. It contains an http.Request. `count` specifies the number
+// of request to be made. `concurrency` defines the number of concurrency i.e how many parallel
+// requests will be made.
 type Request struct {
 	count       int64
 	concurrency int
 	httpRequest *http.Request
 	status      int64
+	mutex       sync.Mutex
 }
 
+// Response contains an http.Response and an error if occured while making the request.
 type Response struct {
 	httpResponse *http.Response
 	err          error
 }
 
+// HttpResponse is the getter method to get the http.Response of a concurrent Request.
 func (res Response) HttpResponse() *http.Response {
 	return res.httpResponse
 }
 
+// Error is the getter method to get the error of a concurrent Request.
 func (res Response) Error() error {
 	return res.err
 }
 
-func NewRequest(httpRequest *http.Request, count int64, concurrency int) (req Request) {
-	return Request{
+// NewRequest is the constructor for concurrent.Request. It takes a defined http.Request,
+// count which specifies the number of request to be made and concurrency of the requests.
+func NewRequest(httpRequest *http.Request, count int64, concurrency int) (req *Request) {
+	return &Request{
 		count:       count,
 		concurrency: concurrency,
 		httpRequest: httpRequest,
@@ -35,25 +44,26 @@ func NewRequest(httpRequest *http.Request, count int64, concurrency int) (req Re
 	}
 }
 
-func (req Request) MakeSync() (res chan Response) {
+// MakeSync makes the given requests in a blocking manner and returns when all the requests
+// have been completed. It returns a channel of Responses correspondong to each request.
+func (req *Request) MakeSync() (res chan Response) {
 	res = make(chan Response, req.count)
 	defer close(res)
 
 	wg := sync.WaitGroup{}
-	mutex := sync.Mutex{}
 
 	for i := 0; i < req.concurrency; i++ {
 		wg.Add(1)
 		go func() {
 			for {
-				mutex.Lock()
+				req.mutex.Lock()
 				if req.status >= req.count {
-					mutex.Unlock()
+					req.mutex.Unlock()
 					break
 				}
 
 				req.status++
-				mutex.Unlock()
+				req.mutex.Unlock()
 				newRes := Response{}
 				newRes.httpResponse, newRes.err = http.DefaultClient.Do(req.httpRequest)
 				res <- newRes
@@ -69,6 +79,10 @@ func (req Request) MakeSync() (res chan Response) {
 	return
 }
 
-func (req Request) Status() (completed float32) {
-	return (float32(atomic.LoadInt64(&req.status)) / float32(req.count)) * 100
+// Status returns the percentage of requests completed.
+func (req *Request) Status() (completed float32) {
+	req.mutex.Lock()
+	defer req.mutex.Unlock()
+	fmt.Println(req.status)
+	return float32(req.status) / float32(req.count) * 100
 }
